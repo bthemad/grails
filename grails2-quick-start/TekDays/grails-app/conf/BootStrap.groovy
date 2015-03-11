f3b3fcb82287250ed02a37cc5a4647494ddf3ee1
Alexander Kudryashov <alexander.kudryashov@softonic.com>
2015-03-11 12:17:38 +0100

    Done with Chapter 3

--- grails2-quick-start/TekDays/grails-app/conf/BootStrap.groovy
+++ grails2-quick-start/TekDays/grails-app/conf/BootStrap.groovy
@@ -1,7 +1,39 @@
+import com.tekdays.TekEvent
+
 class BootStrap {
 
     def init = { servletContext ->
+        def event1 = new TekEvent(
+                name: 'GUG',
+                city: 'Madrid',
+                organizer: 'Joe',
+                venue: 'TBD',
+                startDate: new Date('3/15/2015'),
+                endDate: new Date(('3/15/2015')),
+                description: '''This is a small Groovy user group meeting'''
+        )
+        if (!event1.save()) {
+            event1.errors.allErrors.each { error ->
+                println "An error occured with event1: ${error}"
+            }
+        }
+
+        def event2 = new TekEvent(
+                name: 'Greach',
+                city: 'Madrid',
+                organizer: 'John',
+                venue: 'TBD',
+                startDate: new Date('4/11/2015'),
+                endDate: new Date('4/12/2015'),
+                description: '''Biggest spanish conference for Groovy & Grails related topics'''
+        )
+        if (!event2.save()) {
+            event2.errors.allErrors.each { error ->
+                println "An error occured with event2: ${error}"
+            }
+        }
     }
+
     def destroy = {
     }
 }
--- grails2-quick-start/TekDays/grails-app/domain/com/tekdays/TekEvent.groovy
+++ grails2-quick-start/TekDays/grails-app/domain/com/tekdays/TekEvent.groovy
@@ -4,6 +4,20 @@ class TekEvent {
     String name
     String city
     String organizer
+    String venue
+    Date startDate
+    Date endDate
+    String description
+
+    static constraints = {
+        name()
+        city()
+        description maxSize: 5000
+        organizer()
+        venue()
+        startDate()
+        endDate()
+    }
 
     String toString() {
         "$name, $city"
