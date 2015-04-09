8b951b8b6bf78d4d2333cb24a32c96c3e734e334
Alex Kudryashov <alex.kudryashov@gmail.com>
2015-04-09 10:50:16 +0200

    Groovy version that is magically shorter

--- GroovyHelloWorld/src/HelloWorld.groovy
+++ GroovyHelloWorld/src/HelloWorld.groovy
@@ -1,17 +1,9 @@
 public class HelloWorld {
-    private String name;
+    String name
 
-    public static void main(String[] args) {
-        HelloWorld hw = new HelloWorld();
-        hw.setName("Alex");
-        System.out.println(hw.getName());
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(name) {
-        this.name = name;
+    static void main(String[] args) {
+        HelloWorld hw = new HelloWorld()
+        hw.setName("Alex")
+        println(hw.getName())
     }
 }
